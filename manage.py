b6435a7b73d3e349e4e029febbf4a5a4bc8c2e8d
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-09-26 01:23:23 +0300

    install requests

--- manage.py
+++ manage.py
@@ -1,4 +1,14 @@
-from app import app
+
+from flask import Flask
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    req = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    return 'taraa'
 
 if __name__ == '__main__':
     app.run(debug = True)
