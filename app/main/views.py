53bae57657e9cdeb228e58745d33f1ab1da4da6e
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-09-25 22:22:22 +0300

    edit views.py

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@app.route('/quote/<quote_id>')
+def movie(quote_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('quote.html',id = quote_id)    
