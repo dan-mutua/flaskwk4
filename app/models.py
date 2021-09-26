7106cbc7864f26b2732629eecb69dd5333ca2fe9
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-09-26 20:46:17 +0300

    add multiple quote card

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+
+
 #callback function to retrieve a user when a unique identifier is passed
 @login_manager.user_loader
 def load_user(user_id):
